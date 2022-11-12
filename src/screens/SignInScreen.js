5d0b9293ccf1798c3dd928410a653f5d6e218d03
adbhut96 <21MCA2633@cuchd.in>
2022-11-13 00:49:55 +0530

    commit after UI of sign in form

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,6 +1,13 @@
 import React, {useState} from "react";
+ 
 
 
+
+
+//importing local files
+import HomeScreen from "./HomeScreen";
+import ManAboveForm from '../assets/images/man-logo.png';
+//importing stylesheets
 import "./SignInScreen.css"
 
 
@@ -8,34 +15,56 @@ import "./SignInScreen.css"
 
 function SignInScreen(){
  
-    //states to handle 
-    const [errorMessages , setErrorMessages] = useState({});
-    const [isSubmitted , setIsSubmitted]  = useState({});
-    
+    const [user , setUser] = useState({
+        email:"",
+        password:"",
+   }) 
+   
 
-     //errors which we will get
-     const errors= {
-        email : "invalid email address",
-        pass : "invalid password", 
-     }
+   // function to handle the change in input state
+   const handlingChange=(e)=>{
+       
+       const {name , value} = e.target;
+       console.log(name,value)
+       setUser({
+           ...user,
+           [name]:value
+       }
+       )
+   }
 
-
-    //error message is persist 
-    const renderErrorMessage =(name) => 
-    name ===errorMessages.name &&(
-      <div className="error">{errorMessages.message}</div>
-    );
 
 
     return(
+
+        
         <div className="main-div">
+                 
+              
              
              <div className="form-container">
-                <form>
-                    <div className="input-container">
-                        
-                    </div>
-                    </form>     
+             <img className="man-above-form" src={ManAboveForm} />
+                <div className="text-welcome">
+                    <p className="text">Welcome back! <br/> Please Sign IN to Proceed </p>
+                </div>
+              <div className="form">  
+              <label className="label">
+                Email
+              <input  className="input-field" type="email"  name='email'  value={user.email} placeholder="Your Email" onChange={handlingChange}></input>
+              </label>
+             <label className="label">
+                Password
+             <input className="input-field" type="password" name='password' value={user.password} placeholder="Password" onChange={handlingChange}></input>
+              
+             </label>
+              <div className='log-button'> SIGN IN </div>
+            </div>
+
+            <div className="signup-text">
+                or don't have a account ,  
+                    
+            </div>
+            <div className="underlying-text"><span className="click-here">click here</span> to SIGN UP</div>
              </div>
 
         </div>
